a6dd34a730a982f5b747cee860c61716e6fe4375
Vitaly <vi19954@gmail.com>
2025-08-23 01:44:34 +0300

    feat: project setup and configuration

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test-utils/setup.ts',
+    coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/test/**',
+        'src/**/*.d.ts',
+        'src/App.tsx',
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+});
