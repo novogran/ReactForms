d860a0a31a8b4da1052ef1520aef4e2a76d3a293
Vitaly <vi19954@gmail.com>
2025-08-24 20:46:10 +0300

    feat: add tests for form fields

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test-utils/setup.ts',
+    setupFiles: './src/test/setup.ts',
     coverage: {
       include: ['src/**/*.{js,jsx,ts,tsx}'],
       exclude: [
